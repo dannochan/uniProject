060b23f0b9d8f6db81807a665db5e9c41f111e3c
Danno <desheng.chen@stud.uni-bamberg.de>
2021-05-12 15:15:21 +0200

    basicMethodsAdded

--- Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
+++ Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
@@ -4,6 +4,7 @@ import de.uniba.wiai.dsg.ajp.assignment1.search.SearchTask;
 import de.uniba.wiai.dsg.ajp.assignment1.search.TokenFinder;
 import de.uniba.wiai.dsg.ajp.assignment1.search.TokenFinderException;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +16,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SimpleTokenFinder implements TokenFinder {
 
@@ -31,36 +33,72 @@ public class SimpleTokenFinder implements TokenFinder {
 
         // TODO: Implement here!
 
-        Path pathOfRoot = Paths.get(task.getRootFolder()); // variable for iteration
-        InputStream is = getClass().getClassLoader().getResourceAsStream(task.getIgnoreFile());
+        Path pathOfRoot = Path.of(task.getRootFolder()); // variable for iteration
+        Path pathOfIgnoreFile = Path.of(task.getIgnoreFile());
+
+        //eine Arraylist um die PATHS abzuspeichen
+        List<Path> pathsOfFiles;
 
         if (Files.isDirectory(pathOfRoot)) {
+
             try {
-                DirectoryStream<Path> rootStream = Files.newDirectoryStream(pathOfRoot);
+                Stream<Path> filesWalk = Files.walk(pathOfRoot);
+                List<String> itemsToIgnore = readFile(pathOfIgnoreFile);
+                pathsOfFiles = filesWalk.filter(Files::isDirectory).
+                                    filter(x-> checkIgnoreItem(x, itemsToIgnore)).
+                                            collect(Collectors.toList());
 
-                rootStream.forEach(path -> {
-                    System.out.println(path);
-                });
-                for (Path element :
-                        rootStream) {
-
-                    System.out.println(element.toAbsolutePath());
-                    //iterieren alle die Verzeichniss und Datei
-                    Files.walk(pathOfRoot).forEach(path -> {
-                        System.out.println(path);
-                    });
-
-                }
+                pathsOfFiles.forEach(System.out::println);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-
-
         }
 
+    }
 
+    public static List<Path> directoryWalk(Path path) throws IOException {
+        List<Path> paths = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream
+            ) {
+                if (Files.isDirectory(entry)) {
+                    directoryWalk(entry);
+                }
+                paths.add(entry);
+            }
+            return paths;
+        }
+    }
+        // a method to traverse directories and give back boolean value as result
+    public boolean checkIgnoreItem(Path path, List<String> items){
+        boolean result = false;
+
+        for (String element:
+             items) {
+            if (path.toString().contains(element)){
+                //print to be deleted
+                System.out.println("Directory should be ignored!");
+                result =true;
+            }
+        }
+        return result;
+    }
+        // a Method to read file and give back a list
+    public List<String> readFile(Path filePath){
+        List<String> tmpList = new ArrayList<>();
+        try {
+            BufferedReader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8);
+
+            String line = reader.readLine();
+            while (line!=null){
+                tmpList.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tmpList;
     }
 
 }
