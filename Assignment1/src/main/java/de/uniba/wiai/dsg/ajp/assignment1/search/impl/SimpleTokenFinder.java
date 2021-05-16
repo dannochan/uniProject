82b1a78c637bd2fbcacba1cb35313bae7ae130fb
Desheng Chen <desheng.chen@stud.uni-bamberg.de>
2021-05-16 14:43:34 +0200

    improve the name filtering method

--- Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
+++ Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
@@ -31,20 +31,21 @@ public class SimpleTokenFinder implements TokenFinder {
     @Override
     public void search(final SearchTask task) throws TokenFinderException {
 
-        // TODO: Implement here!
 
-        Path pathOfRoot = Path.of(task.getRootFolder()); // variable for iteration
+        // Variable für die Path von Wurzelverzeichniss und s
+        Path pathOfRoot = Path.of(task.getRootFolder());
         File ignoreFile = new File(task.getIgnoreFile());
 
         try {
             List<String> ignoredItems = Files.readAllLines(ignoreFile.toPath(), StandardCharsets.UTF_8);
 
 
-
-            if (pathOfRoot==null && pathOfRoot.getFileName()==null) {
-                // do something
-            }
             List<Path> listOfRoot = findAndFilterDirectory(pathOfRoot, ignoredItems, task.getFileExtension());
+
+            for (Path entry: listOfRoot
+                 ) {
+                System.out.println(entry.toString());
+            }
 
 
         }catch (IOException e){
@@ -63,28 +64,34 @@ public class SimpleTokenFinder implements TokenFinder {
         }
 
         try (Stream<Path> treeWalk = Files.walk(path)){
-            result = treeWalk.filter(Files::isRegularFile).
+            result = treeWalk.filter(Files::isDirectory).
                                     filter(path1 -> !checkNameOfFile(path1, list)).
-                                        filter(path1 -> path1.getFileName().toString().endsWith(fileExtension)).
+                                      //  filter(path1 -> path1.getFileName().toString().endsWith(fileExtension)).
                                            collect(Collectors.toList());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-
+    // ein Method, um die Directories mit den in der angegebenen File steheden Name zu filtern
     private static boolean checkNameOfFile(Path pathForCheck, List<String> listForCheck){
 
             boolean result = false;
             for (String elem : listForCheck) {
                 if (pathForCheck.toAbsolutePath().toString().contains(elem)){
+                    if (pathForCheck.getFileName().toString().endsWith(elem)) {
+                        System.out.format("[%s] directory was ignored.%n", pathForCheck.getFileName());
+                    }
                     result = true;
+
                 }
 
             }
             return result;
     }
+
 
 
 }
