5264eb93e222fb53cce95c893db49932183e00fe
Desheng Chen <desheng.chen@stud.uni-bamberg.de>
2021-05-17 14:57:19 +0200

    new Method for creation of output file added

--- Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
+++ Assignment1/src/main/java/de/uniba/wiai/dsg/ajp/assignment1/search/impl/SimpleTokenFinder.java
@@ -5,15 +5,15 @@ import de.uniba.wiai.dsg.ajp.assignment1.search.TokenFinder;
 import de.uniba.wiai.dsg.ajp.assignment1.search.TokenFinderException;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -28,58 +28,42 @@ public class SimpleTokenFinder<TODO> implements TokenFinder {
          */
     }
 
+
     @Override
     public void search(final SearchTask task) throws TokenFinderException {
 
 
-        // Variable für die Path von Wurzelverzeichniss und s
+        // Variable für die Path von Wurzelverzeichniss und die Ignore-File
         Path pathOfRoot = Path.of(task.getRootFolder());
         File ignoreFile = new File(task.getIgnoreFile());
+
 
         try {
             List<String> ignoredItems = Files.readAllLines(ignoreFile.toPath(), StandardCharsets.UTF_8);
 
-
-            List<Path> listOfRoot = findAndFilterDirectory(pathOfRoot, ignoredItems, task.getFileExtension());
-
-            for (Path path: listOfRoot
-                 ) {
-                System.out.println(path.toString());
-            }
-
-            //readFiles(listOfRoot);
+            List<Path> listOfRoot = findAndFilterDirectory(pathOfRoot, ignoredItems, task.getFileExtension(), task.getResultFile());
 
 
-        }catch (IOException e){
+            readFiles(listOfRoot, task.getToken());
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-
     }
 
-    private void readFiles(List<Path> listOfRoot, String extensionName) {
 
-        for (Path element: listOfRoot
-             ) {
-
-            element.getFileName();
-        }
-
-    }
-
-    private static List<Path> findAndFilterDirectory(Path path, List<String> list, String fileExtension) throws TokenFinderException {
+    public List<Path> findAndFilterDirectory(Path path, List<String> list, String fileExtension, String resultFile) throws TokenFinderException {
         List<Path> result = null;
 
-        if(!Files.isDirectory(path)){
+        if (!Files.isDirectory(path)) {
             throw new TokenFinderException("The given path is not an available root folder!");
         }
 
-        try (Stream<Path> treeWalk = Files.walk(path)){
+        try (Stream<Path> treeWalk = Files.walk(path)) {
             result = treeWalk.filter(Files::isReadable).
-                                    filter(path1 -> !checkNameOfFile(path1, list)).
-                                        filter(p->p.getFileName().toString().endsWith(fileExtension)).
-                                           collect(Collectors.toList());
+                    filter(path1 -> !checkNameOfFile(path1, list, resultFile)).
+                    filter(p -> p.getFileName().toString().endsWith(fileExtension)).
+                    collect(Collectors.toList());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -88,26 +72,77 @@ public class SimpleTokenFinder<TODO> implements TokenFinder {
     }
 
     // ein Method, um die Directories mit den in der angegebenen File steheden Name zu filtern
-    private static boolean checkNameOfFile(Path pathForCheck, List<String> listForCheck){
+    private static boolean checkNameOfFile(Path pathForCheck, List<String> listForCheck, String resultFile) {
+        List<String> outputArray = new ArrayList<>();
+        boolean result = false;
+        for (String elem : listForCheck) {
+            if (pathForCheck.toAbsolutePath().toString().contains(elem)) {
 
-            boolean result = false;
-            for (String elem : listForCheck) {
-                if (pathForCheck.toAbsolutePath().toString().contains(elem)){
-                    if (pathForCheck.getFileName().toString().endsWith(elem)) {
-                        System.out.format("[%s] directory was ignored.%n", pathForCheck.getFileName());
-                    }
-                    result = true;
+                if (pathForCheck.getFileName().toString().endsWith(elem)) {
+                    String outputLine = pathForCheck + " directory was ignored.";
+
+                    outputArray.add(outputLine);
 
                 }
-
+                result = true;
             }
-            return result;
+
+        }
+        writeToFileNewLines(resultFile, outputArray);
+        return result;
     }
 
- // eine Method, um die Datei lesen zu können
-   // TODO implement here
-
-
- // eine Method, um die Ausgabe in eine Datei speichern zu können.
+    // eine Method, um die Datei lesen zu können
     // TODO implement here
+    public static void readFiles(List<Path> pathList, String keyword) {
+        List<String> outoutTmp = new ArrayList<>();
+
+        for (Path elem : pathList
+        ) {
+
+            try (Stream<String> lines = Files.lines(elem)) {
+
+                System.out.println(elem.toString() + " is the path of " +elem.getFileName());
+             //   System.out.println(lines.filter(x -> !x.contains(keyword)).distinct().count());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private static void countOccurence(String x, String keyword) {
+
+    }
+    // eine Method, damit die Ergebniss in eine Text-Datei gespeichert werden.
+    // TODO implement here
+
+    public static void writeToFileNewLines(String outputFile, List<String> newLines) {
+
+        Path path = Path.of(outputFile); // Die Text-Datei, die zu bearbeiten ist.
+
+        if (Files.exists(path)) {          // prüfen, ob die Datei schon existiert.
+            //Falls die Output Datei schon existiert, werden die neuen Line in Datei reingeschrien
+            try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
+
+                for (String elem : newLines) {
+
+                    writer.write(elem);
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        // Falls die Datei noch nicht existiert, wird zuerst eine Text-File erstellt.
+        } else {
+            try {
+                Files.write(path, newLines, StandardOpenOption.CREATE_NEW);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
