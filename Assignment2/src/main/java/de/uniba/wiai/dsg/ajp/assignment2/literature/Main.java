72ed82ec33181c2a87c598aba07baa9849991fc1
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-06 19:05:30 +0200

    Implementierung der DatabaseServiceImpl-Funktionen

--- Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/Main.java
+++ Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/Main.java
@@ -1,12 +1,55 @@
 package de.uniba.wiai.dsg.ajp.assignment2.literature;
 
 
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.DatabaseService;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.LiteratureDatabaseException;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.MainService;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.impl.MainServiceImpl;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Author;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Publication;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.PublicationType;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+import static java.lang.System.gc;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO startet eure Anwendung ueber diese main-Methode
-		System.out.println("Something happend");
-		//Test-Kommentar fuer Git
-	}
+    public static void main(String[] args) {
+        // TODO startet eure Anwendung ueber diese main-Methode
+        MainService startIt = new MainServiceImpl();
+        try {
+
+            DatabaseService mydatabase = startIt.create();
+
+            mydatabase.addAuthor("Hans Petter Oechsler", "demhansiseiadress", "a0");
+            mydatabase.addAuthor("Hans Petter Oechsler", "demhansiseiadress", "a1");
+            mydatabase.addAuthor("Erwin Schnuerschuh","demerwinseiadress","a2");
+
+            List<String> liste = new LinkedList<>();
+            liste.add("a1");
+            liste.add("a2");
+            mydatabase.addPublication("Husten hat er der Hansi", 2020, PublicationType.BOOK, liste, "p1");
+            mydatabase.addPublication("Gartensachen", 2021, PublicationType.BOOK, liste, "p2");
+
+            mydatabase.removePublicationByID("p2");
+            //garbage colletion anstossen
+            gc();
+            //System.out.println(mydatabase.getPublications());
+            //System.out.println(mydatabase.getAuthors());
+            //ListIterator<Author> a = mydatabase.getAuthors().listIterator();
+
+
+            //System.out.println(a.next().getPublications().listIterator().next().getAuthors());
+            //System.out.println(a.next().getPublications());
+            //System.out.println(mydatabase.getAuthors().listIterator().next().getPublications());
+        } catch (LiteratureDatabaseException e) {
+            System.out.println("Schrottbude");
+        }
+
+    }
 
 }
--- Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/logic/impl/DatabaseServiceImpl.java
+++ Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/logic/impl/DatabaseServiceImpl.java
@@ -1,6 +1,8 @@
 package de.uniba.wiai.dsg.ajp.assignment2.literature.logic.impl;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.DatabaseService;
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.LiteratureDatabaseException;
@@ -9,75 +11,232 @@ import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Database;
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Publication;
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.PublicationType;
 
+
 public class DatabaseServiceImpl implements DatabaseService {
 
-	private Database database;
+    private Database database;
 
-	DatabaseServiceImpl(){
+    DatabaseServiceImpl() {
 
-	}
+    }
 
-	DatabaseServiceImpl(Database database){
-		this.database = database;
-	}
+    DatabaseServiceImpl(Database database) {
+        this.database = database;
+    }
 
-	DatabaseServiceImpl(List<Database> databases){
+    DatabaseServiceImpl(List<Database> databases) {
 
-	}
+    }
 
-	@Override
-	public void addPublication(String title, int yearPublished, PublicationType type, List<String> authorIDs, String id)
-			throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+    @Override
+    public void addPublication(String title, int yearPublished, PublicationType type, List<String> authorIDs, String id)
+            throws LiteratureDatabaseException {
 
-	}
+        //TODO: Exceptions einfügen, Email Validierung... Fehler: Wenn Author noch nicht existiert usw.
 
-	@Override
-	public void removePublicationByID(String id) throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+        Publication freshPublication = new Publication();
 
-	}
+        freshPublication.setId(id);
+        freshPublication.setTitle(title);
+        freshPublication.setYearPublished(yearPublished);
+        freshPublication.setType(type);
 
-	@Override
-	public void removeAuthorByID(String id) throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+        //umwandeln der Autorenliste, String to Obj.
+        List<Author> currentAuthors = getAuthorObjRefFormStringList(authorIDs);
+        freshPublication.setAuthors(currentAuthors);
 
-	}
 
-	@Override
-	public void addAuthor(String name, String email, String id) throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+        //einfuegen der neuen Publication in die Liste der Publicationen (der aktuellen Datenbank)
+        getPublications().add(freshPublication);
 
-	}
+        //neue Publication bei Autoren eintragen
+        ListIterator<Author> currentAuthorIterator = currentAuthors.listIterator();
+        while (currentAuthorIterator.hasNext()) {
+            currentAuthorIterator.next().getPublications().add(freshPublication);
+        }
+    }
 
-	@Override
-	public List<Publication> getPublications() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private List<Author> getAuthorObjRefFormStringList(List<String> authorsIDs) {
 
-	@Override
-	public List<Author> getAuthors() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        //empty Result-List to be returned
+        List<Author> currentAuthors = new LinkedList<>();
 
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
+        //getting List-Iterators for while-loop
+        ListIterator<Author> allAuthorsIterator = getAuthors().listIterator();
+        ListIterator<String> stringAuthorIterator = authorsIDs.listIterator();
 
-	}
+        while (allAuthorsIterator.hasNext()) {
+            //get current IDs
+            String allAuthorID = allAuthorsIterator.next().getId();
+            String stringAuthorID = stringAuthorIterator.next();
 
-	@Override
-	public void printXMLToConsole() throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+            if (allAuthorID.equals(stringAuthorID)) {
+                //in currentAuthor abspeichern
+                Author authorRef = allAuthorsIterator.previous();
+                currentAuthors.add(authorRef);
+                allAuthorsIterator.next();
 
-	}
+            } else {
+                stringAuthorIterator.previous();
+            }
+        }
 
-	@Override
-	public void saveXMLToFile(String path) throws LiteratureDatabaseException {
-		// TODO Auto-generated method stub
+        //TODO: wenn Author noch nicht in database vorhanden dann neuen anlegen. Implementierung  als if statement in while Schleife oben
 
-	}
+        return currentAuthors;
+
+    }
+
+    @Override
+    public void removePublicationByID(String id) throws LiteratureDatabaseException {
+        // TODO Auto-generated method stub
+        //aus Liste der Publicationen entfernen
+
+        List<Publication> deleteHelper = getPublications();
+        ListIterator<Publication> deleteHelperIterator = deleteHelper.listIterator();
+        List<Author> ListOfPublicationAuthors = new LinkedList<>();
+
+        //get current Publication ID
+        String deleteID = deleteHelperIterator.next().getId();
+
+        boolean condition = true;
+
+        while (condition) {
+            if (id.equals(deleteID)) {
+
+                // Liste der Autoren die mit der Publikation zu tun haben
+                ListOfPublicationAuthors = deleteHelperIterator.previous().getAuthors();
+
+                //zuruecksetzen des Iterators zum nachfolgenden loeschen
+                deleteHelperIterator.next();
+
+                //Loeschen der Publication aus Liste
+                deleteHelperIterator.remove();
+
+
+                //Schleife abbrechen
+                condition = false;
+            } else {
+                //Schleife abbrechen, wenn die Liste aller Publications zu Ende ist
+                if (!deleteHelperIterator.hasNext()) {
+                    condition = false;
+                } else {
+                    deleteID = deleteHelperIterator.next().getId();
+                }
+            }
+        }
+
+
+        //<Publication aus Autoren-Obj. entfernen
+
+        //shows the current Author-Obj., where to search for the Publication to be removed
+        ListIterator<Author> loPAIterator = ListOfPublicationAuthors.listIterator();
+
+        //re-use of deleting Helper Var., shows the Obj. to be removed
+        deleteHelperIterator = loPAIterator.next().getPublications().listIterator();
+        loPAIterator.previous();
+
+        while(loPAIterator.hasNext()){
+            condition = true;
+            //neudeklatration der Helper-Var., da LoPAIterator auf andere Stelle zeigt (geht nicht ohne)
+            deleteHelperIterator = loPAIterator.next().getPublications().listIterator();
+            loPAIterator.previous();
+            //auslesen der aktuellen Id in der Liste
+            deleteID = deleteHelperIterator.next().getId();
+
+            while (condition) {
+                if (id.equals(deleteID)) {
+                    //Loeschen der Publication aus Liste
+                    deleteHelperIterator.remove();
+                    //Schleife abbrechen
+                    condition = false;
+                } else {
+                    //Schleife abbrechen, wenn die Liste aller Publications zu Ende ist
+                    if (!deleteHelperIterator.hasNext()) {
+                        condition = false;
+                    } else {
+                        deleteID = deleteHelperIterator.next().getId();
+                    }
+                }
+            }
+            loPAIterator.next();
+        }
+
+
+
+    }
+
+    @Override
+    public void removeAuthorByID(String id) throws LiteratureDatabaseException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addAuthor(String name, String email, String id) throws LiteratureDatabaseException {
+
+        Author newAuthor = new Author();
+        newAuthor.setName(name);
+        newAuthor.setEmail(email);
+        newAuthor.setId(id);
+
+        //TODO: Eingabedaten ueberpruefen, wo? Author-Setter oder hier?
+
+        List<Author> inputHelper = getAuthors();
+        inputHelper.add(newAuthor);
+
+    }
+
+    @Override
+    public List<Publication> getPublications() {
+        // TODO Auto-generated method stub
+        List<Publication> listOfAllPublications = new LinkedList<>();
+        listOfAllPublications = this.database.getPublications();
+        return listOfAllPublications;
+    }
+
+    @Override
+    public List<Author> getAuthors() {
+        // TODO Auto-generated method stub
+        List<Author> listOfAllAuthors = new LinkedList<>();
+        listOfAllAuthors = this.database.getAuthors();
+        return listOfAllAuthors;
+    }
+
+    @Override
+    public void clear() {
+        List<Publication> publications = getPublications();
+        publications.clear();
+        List<Author> authors = getAuthors();
+        authors.clear();
+    }
+
+    @Override
+    public void printXMLToConsole() throws LiteratureDatabaseException {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Database.class);
+            Marshaller ms = context.createMarshaller();
+            ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            ms.marshal(database, System.out);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void saveXMLToFile(String path) throws LiteratureDatabaseException {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Database.class);
+            Marshaller ms = context.createMarshaller();
+            ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            ms.marshal(database, Path.of(path).toFile());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
