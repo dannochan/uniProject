4ab19e5f0fc0cce612964e5f5876ba8476eb37fd
Hüsnü <ba040892>
2021-06-08 21:13:32 +0200

    Final Changes Main Class v2

--- Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/Main.java
+++ Assignment2/src/main/java/de/uniba/wiai/dsg/ajp/assignment2/literature/Main.java
@@ -1,19 +1,10 @@
 package de.uniba.wiai.dsg.ajp.assignment2.literature;
 
 
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.DatabaseService;
+
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.LiteratureDatabaseException;
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.MainService;
 import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.impl.ConsoleUI;
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.impl.MainServiceImpl;
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Author;
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.Publication;
-import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model.PublicationType;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import static java.lang.System.gc;
 
 public class Main {
 
