43be602e7bebd7f071003b88b5f07ff5bb60f86d
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-15 13:16:19 +0200

    Apply new project source from vc

---
+++ Assignment3/src/main/java/de/uniba/wiai/dsg/ajp/assignment3/Customer.java
@@ -0,0 +1,90 @@
+package de.uniba.wiai.dsg.ajp.assignment3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+
+	private List<Rental> rentals = new LinkedList<Rental>();
+
+	public Customer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Rental> getRentals() {
+		return rentals;
+	}
+
+	public void setRentals(List<Rental> rentals) {
+		this.rentals = rentals;
+	}
+
+	public String statement() {
+		String result = "Rental Record for " + getName() + "\n";
+
+		int frequentRenterPoints = 0;
+		for (Rental each : this.rentals) {
+			frequentRenterPoints += each.getFrequentRenterPoints();
+
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t"
+					+ String.valueOf(each.getCharge()) + "\n";
+		}
+
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints)
+				+ " frequent renter points";
+		return result;
+	}
+
+	public String htmlStatement() {
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+		for (Rental each : rentals) {
+			// show figures for each rental
+			result += each.getMovie().getTitle() + ": "
+					+ String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge())
+				+ "</EM><P>\n";
+		result += "On this rental you earned <EM>"
+				+ String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
+
+	double getTotalCharge() {
+		double result = 0;
+
+		for (Rental each : rentals) {
+			result += each.getCharge();
+		}
+
+		return result;
+	}
+
+	int getTotalFrequentRenterPoints() {
+		int result = 0;
+
+		for (Rental each : rentals) {
+			result += each.getFrequentRenterPoints();
+		}
+
+		return result;
+	}
+
+}
