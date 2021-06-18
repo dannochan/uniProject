08973e73d473d9aa3d552b41f85695ec189ce15c
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-18 18:59:32 +0200

    back to the beginning -2ter Versuch

--- Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/ChildrensPriceTest.java
+++
@@ -1,8 +0,0 @@
-package java.de.uniba.wiai.dsg.ajp.assignment3;
-
-public class ChildrensPriceTest {
-    //TODO testing of: (default) getCharge(int daysRented)
-
-    //TODO testing of: (default) getPriceCode() -> Klaeren ob es sich um eine Standart-Getter-Methode handelt?
-
-}
--- Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/MovieTest.java
+++
@@ -1,8 +0,0 @@
-package de.uniba.wiai.dsg.ajp.assignment3;
-
-public class MovieTest {
-
-    //TODO testing of: public void setPrieCode()
-    
-
-}
