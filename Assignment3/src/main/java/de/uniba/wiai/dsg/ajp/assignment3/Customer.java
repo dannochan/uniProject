edd5a9f2a9283dcb8df016efca0d2d985981df44
Danno <desheng.chen@stud.uni-bamberg.de>
2021-06-20 19:46:21 +0200

    String.valueOf in method statement() deleted

--- Assignment3/src/main/java/de/uniba/wiai/dsg/ajp/assignment3/Customer.java
+++ Assignment3/src/main/java/de/uniba/wiai/dsg/ajp/assignment3/Customer.java
@@ -30,6 +30,7 @@ public class Customer {
 		this.rentals = rentals;
 	}
 
+	// String.valueOf werden löscht.
 	public String statement() {
 		String result = "Rental Record for " + getName() + "\n";
 
@@ -39,12 +40,12 @@ public class Customer {
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(each.getCharge()) + "\n";
+					+ each.getCharge() + "\n";
 		}
 
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
+		result += "Amount owed is " + getTotalCharge() + "\n";
+		result += "You earned " + frequentRenterPoints
 				+ " frequent renter points";
 		return result;
 	}
