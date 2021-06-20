ee6b171825fc41a3b740d4013b3e2c09f33cd66a
Danno <desheng.chen@stud.uni-bamberg.de>
2021-06-21 00:43:37 +0200

    added more movie test instance to test statement()

--- Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/CustomerTest.java
+++ Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/CustomerTest.java
@@ -1,15 +1,12 @@
 package de.uniba.wiai.dsg.ajp.assignment3;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,25 +20,6 @@ public class CustomerTest {
 
     }
 
-    @Test
-     public void giveBackTheNameOfTheCustomer(){
-
-         //given
-       //  Customer customer = new Customer()
-
-         //when
-         String outputName = customer.getName();
-
-         //then
-         assertEquals("Bob", outputName, "The name should be Bob");
-
-     }
-
-
-    @Test
-    @Disabled
-    void setRentals() {
-    }
 
     @Test
     void statementShouldBeGivenOutInCorrectFormat() {
@@ -53,12 +31,26 @@ public class CustomerTest {
         when(movieMocke.getPriceCode()).thenReturn(1);
         when(movieMocke.getCharge(3)).thenReturn(23.99);
 
+
         Rental rental = mock(Rental.class);
         when(rental.getMovie()).thenReturn(movieMocke);
         when(rental.getDaysRented()).thenReturn(3);
         when(rental.getCharge()).thenReturn(23.99);
+        when(rental.getFrequentRenterPoints()).thenReturn(1);
+
+        Movie movieMocked1 = mock(Movie.class);
+        when(movieMocked1.getTitle()).thenReturn("Harry Potter 2");
+        when(movieMocked1.getPriceCode()).thenReturn(1);
+        when(movieMocked1.getCharge(3)).thenReturn(23.99);
+
+        Rental rental1 = mock(Rental.class);
+        when(rental1.getMovie()).thenReturn(movieMocked1);
+        when(rental1.getDaysRented()).thenReturn(3);
+        when(rental1.getCharge()).thenReturn(23.99);
+        when(rental1.getFrequentRenterPoints()).thenReturn(1);
 
         testList.add(rental);
+        testList.add(rental1);
 
         customer.setRentals(testList);
 
@@ -69,7 +61,6 @@ public class CustomerTest {
         System.out.println(resultTest);
 
         // then
-
 
     }
 
