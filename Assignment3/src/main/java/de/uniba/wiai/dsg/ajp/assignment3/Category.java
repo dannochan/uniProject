139ce800023957fd226b15a652f2eab63d9170e1
Danno <desheng.chen@stud.uni-bamberg.de>
2021-06-21 01:41:37 +0200

    add new price category LOW_BUDGET

--- Assignment3/src/main/java/de/uniba/wiai/dsg/ajp/assignment3/Category.java
+++ Assignment3/src/main/java/de/uniba/wiai/dsg/ajp/assignment3/Category.java
@@ -3,7 +3,8 @@ package de.uniba.wiai.dsg.ajp.assignment3;
 public enum Category {
     REGULAR(0),
     NEW_RELEASE(1),
-    CHILDRENS(2);
+    CHILDRENS(2),
+    LOW_BUDGET(3);
 
     private final int value;
 
