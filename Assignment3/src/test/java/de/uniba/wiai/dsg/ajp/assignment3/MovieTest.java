553a370b5a8926a79c12e6739204c43a8f50b883
Danno <desheng.chen@stud.uni-bamberg.de>
2021-06-22 00:08:40 +0200

    test code for MovieTest class because of change in Movie class

--- Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/MovieTest.java
+++ Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/MovieTest.java
@@ -1,9 +1,27 @@
 package de.uniba.wiai.dsg.ajp.assignment3;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MovieTest {
 
     private Movie movie;
+    private Movie movie1;
 
+    @BeforeEach
+    public void setUp(){
+        movie=new Movie("His dark Material", 1, "hd");
+       movie1=new Movie("His dark Material", 1, "4k");
+    }
 
+    @Test
+    public void setMovieShouldGiveOutRightQuality(){
+
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getCharge(3));
+        System.out.println(movie1.getTitle());
+        System.out.println(movie1.getCharge(3));
+
+    }
 
 }
