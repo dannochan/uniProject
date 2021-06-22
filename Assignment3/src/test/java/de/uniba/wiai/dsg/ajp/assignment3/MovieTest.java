8e1dea413bcdd7be4ee347783ea4e3cca5ed3c7a
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-22 20:17:02 +0200

    nice and cleaaaaaaaaaaaaan wuhu

--- Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/MovieTest.java
+++ Assignment3/src/test/java/de/uniba/wiai/dsg/ajp/assignment3/MovieTest.java
@@ -15,44 +15,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MovieTest {
 
     private Movie movie;
-/*
-    @ParameterizedTest
-    @MethodSource("differentPictureQuality")
-    public void getChargeReturnCorrectValue(PictureQuality picturequality, double expected) {
-        //given
 
-        Movie movie = new Movie("Sit", 2,"4K");
-
-        // when
-        double result = movie.getCharge(2);
-
-        // then
-        assertEquals(result, expected);
-    }
-
-    public static List<Arguments> differentPictureQuality() {
-
-        return List.of(Arguments.of(PictureQuality.Resolution_HD, 2),
-                Arguments.of(PictureQuality.Resolution_4k, 4));
-
-
-    }
-*/
     @BeforeEach
-    private void setUp(){
-        movie = new Movie("Sit",2,"4K");
+    private void setUp() {
+        movie = new Movie("Sit", 2, "4K");
     }
 
     @AfterEach
-    private void tearDown(){
+    private void tearDown() {
         movie = null;
     }
 
     @ParameterizedTest
-    @ValueSource(ints={1,2,5,10})
-    public void getChargeReturnsPositveNumber(int daysRented){
+    @ValueSource(ints = {1, 2, 5, 10})
+    public void getChargeReturnsPositveNumber(int daysRented) {
         double result = movie.getCharge(daysRented);
-        assertTrue(result>0,"Method getCharge returns negative Number.");
+        assertTrue(result > 0, "Method getCharge returns negative Number.");
 
     }
 
@@ -65,13 +43,10 @@ public class MovieTest {
     }
 
     @Test
-    public void getTitleInCorrectFormat(){
-        assertEquals("Sit (4K)",movie.getTitle(), "Method getTitle() does not print in correct Format.");
+    public void getTitleInCorrectFormat() {
+        assertEquals("Sit (4K)", movie.getTitle(), "Method getTitle() does not print in correct Format.");
 
     }
-
-
-
 
 
 }
