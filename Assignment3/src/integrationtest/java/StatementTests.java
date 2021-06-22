25650d1f3ce3b9ee6e772d1d6dbce6cb9be4c580
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-22 20:05:42 +0200

    impl. of htmlStatementWorksCorrectly

--- Assignment3/src/integrationtest/java/StatementTests.java
+++ Assignment3/src/integrationtest/java/StatementTests.java
@@ -39,8 +39,13 @@ public class StatementTests {
 
 
     @Test
-    public void statementPrintsCorrectly(){
+    public void statementWorksCorrectly(){
         assertEquals(expectedOutput(), customer.statement(), "Methode statement prints correctly");
+    }
+
+    @Test
+    public void htmlStatementWorksCorrectly(){
+        assertEquals(expectedHtmlOutput(), customer.htmlStatement(), "Methode statement prints correctly");
     }
 
     private String expectedOutput(){
@@ -49,6 +54,15 @@ public class StatementTests {
         resultStatement += "Amount owed is 2.8" + "\n";
         resultStatement += "You earned 1 frequent renter points";
         return resultStatement;
+    }
+
+    private String expectedHtmlOutput() {
+
+        String resultHtmlStatement = "<H1>Rentals for <EM>Bob" + "</EM></H1><P>\n";
+        resultHtmlStatement += "Sit (4K): " + "3.5 20 2.8" + "<BR>\n";
+        resultHtmlStatement += "<P>You owe <EM>2.8" + "</EM><P>\n";
+        resultHtmlStatement += "On this rental you earned <EM>1" + "</EM> frequent renter points<P>";
+        return resultHtmlStatement;
     }
 
 }
