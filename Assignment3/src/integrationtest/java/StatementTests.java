f1e05e0abde7dc6ba6ffdceb386fd710a206aa08
Andreas Haderlein <andreas-frank.haderlein@stud.uni-bamberg.de>
2021-06-22 20:01:32 +0200

    beforEach + AfterEach

--- Assignment3/src/integrationtest/java/StatementTests.java
+++ Assignment3/src/integrationtest/java/StatementTests.java
@@ -1,6 +1,9 @@
 package de.uniba.wiai.dsg.ajp.assignment3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -9,28 +12,35 @@ import java.util.List;
 
 public class StatementTests {
     private Customer customer;
+    private Rental rental;
+    private Movie movie;
 
-
-    @Test
-    public void statementPrintsCorrectly(){
-
+    @BeforeEach
+    private void setUp(){
         customer = new Customer("Bob");
         List<Rental> rentalList = new LinkedList<>();
-        Rental rental = new Rental();
+        rental = new Rental();
         rentalList.add(rental);
 
-        Movie movie = new Movie("Sit", 2, "4K");
+        movie = new Movie("Sit", 2, "4K");
 
         rental.setMovie(movie);
         rental.setDaysRented(3);
         rental.setDiscount(20);
 
         customer.setRentals(rentalList);
-        //String newOne = customer.statement();
-        double hansi = rental.getDiscountedCharge();
+    }
+    @AfterEach
+    private void tearDown(){
+        customer = null;
+        rental = null;
+        movie = null;
+    }
+
+
+    @Test
+    public void statementPrintsCorrectly(){
         assertEquals(expectedOutput(), customer.statement(), "Methode statement prints correctly");
-
-
     }
 
     private String expectedOutput(){
